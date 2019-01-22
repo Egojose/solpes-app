0e93fb1d9555a94c336cfc460e32459813713024
Luis Alejandro Soto Bedoya <luis.soto.itm@gmail.com>
2019-01-22 17:04:13 -0500

    editar solicitud

--- src/app/dominio/condicionContractual.ts
+++ src/app/dominio/condicionContractual.ts
@@ -2,9 +2,12 @@ export class CondicionContractual {
 
     nombre: string;
     id: number;
+    valor:string;
+    
 
-    constructor(nombre: string, id: number) {
+    constructor(nombre: string, id: number, valor?: string) {
         this.nombre = nombre;
         this.id = id;
+        this.valor = valor;
     }
 }
