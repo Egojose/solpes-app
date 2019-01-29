841503a5191c0430126d1b8ee8ca10d718450502
Egojose <josemicrobiologo@gmail.com>
2019-01-29 12:32:06 -0500

    se aplica método para traer adjunto en recepcion bienes sia

--- src/app/recepcion-sap/RecepcionBienes.ts
+++ src/app/recepcion-sap/RecepcionBienes.ts
@@ -1,10 +1,48 @@
 export class RecepcionBienes {
-    constructor(public Idbienes:number ,public descripcion:string, public cantidad:number, public valor:string, public ultimaEntrega:boolean, public comentario?:string, public IdRecepcionBienes?:number, public ultimaEntregaCTB?:boolean, public Author?: any, public Editor?: any, public NumeroRecepcion?: string, public recibidoSap?: boolean){
-
-    }
+    constructor(
+        public Idbienes:number ,
+        public descripcion:string, 
+        public cantidad:number, 
+        public valor:string, 
+        public ultimaEntrega:boolean, 
+        public comentario?:string, 
+        public IdRecepcionBienes?:number, 
+        public ultimaEntregaCTB?:boolean, 
+        public Author?: any, 
+        public Editor?: any, 
+        public NumeroRecepcion?: string, 
+        public recibidoSap?: boolean,
+        public adjunto?: any){}
 
     public static fromJson(element: any) {
-        return new RecepcionBienes(element.IdCTBienesId,element.Descripcion, element.Cantidad, element.Valor, element.UltimaEntrega, element.Comentario, element.Id, element.Editor, element.NumeroRecepcion, element.recibidoSap);
+        
+        let RutaArchivo = "";
+        if (element.Attachments ===true) {
+           let ObjArchivos = element.AttachmentFiles.results;
+            
+           ObjArchivos.forEach(element => {
+               let objSplit = element.FileName.split("-");
+               if (objSplit.length>0) {
+                   let TipoArchivo = objSplit[0]
+                   if (TipoArchivo==="EntregaBienes") {
+                        RutaArchivo=element.ServerRelativeUrl;
+                   }                
+               }
+           });
+        }
+
+        return new RecepcionBienes(
+            element.IdCTBienesId,
+            element.Descripcion, 
+            element.Cantidad, 
+            element.Valor, 
+            element.UltimaEntrega, 
+            element.Comentario, 
+            element.Id, 
+            element.Editor, 
+            element.NumeroRecepcion, 
+            element.recibidoSap,
+            RutaArchivo);
     }
 
     public static fromJsonList(elements: any) {
