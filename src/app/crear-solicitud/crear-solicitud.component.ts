56f27dc34ce87835b49c0353f825fb8bf68f4b6a
Egojose <josemicrobiologo@gmail.com>
2019-03-19 13:39:12 -0500

    cambios menores

--- src/app/crear-solicitud/crear-solicitud.component.ts
+++ src/app/crear-solicitud/crear-solicitud.component.ts
@@ -789,7 +789,7 @@ export class CrearSolicitudComponent implements OnInit {
         let textoCajon = this.solpFormulario.controls['condicionContractual' + condicionContractual.id].value;
         if (textoCajon != null) {
           var json = textoCajon.replace(/[|&;$%@"<>()+,]/g, "");
-          this.jsonCondicionesContractuales = json.replace(/(\r\n|\n|\r|\t)/gm,"");
+          this.jsonCondicionesContractuales = json.replace(/(\r\n|\n|\r|\t)/gm," ");
           this.cadenaJsonCondicionesContractuales += ('{"campo": "' + condicionContractual.nombre + '", "descripcion": "' + this.jsonCondicionesContractuales + '"},');
         }
       });
