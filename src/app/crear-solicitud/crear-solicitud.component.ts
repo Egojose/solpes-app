8f2c8d8f51d10a3e755b78b91c810f8c1dc7d763
Egojose <josemicrobiologo@gmail.com>
2019-05-22 13:15:12 -0500

    agrega caracter especial a listar de reemplazables

--- src/app/crear-solicitud/crear-solicitud.component.ts
+++ src/app/crear-solicitud/crear-solicitud.component.ts
@@ -1703,7 +1703,7 @@ validarCodigosBrasilCTS(codigoValidar, i) {
       this.condicionesContractuales.forEach(condicionContractual => {
         let textoCajon = this.solpFormulario.controls['condicionContractual' + condicionContractual.id].value;
         if (textoCajon != null) {
-          var json = textoCajon.replace(/[|&;$%@"<>()+,]/g, "");
+          var json = textoCajon.replace(/[|&;$%@"<>()+•,]/g, "");
           this.jsonCondicionesContractuales = json.replace(/(\r\n|\n|\r|\t)/gm," ");
           this.cadenaJsonCondicionesContractuales += ('{"campo": "' + condicionContractual.nombre + '", "descripcion": "' + this.jsonCondicionesContractuales + '"},');
         }
