3a4c11e8f5d2f2beee8f71dd111c1fc422e4b831
Egojose <josemicrobiologo@gmail.com>
2019-10-08 11:27:44 -0500

    cambio menor

--- src/app/editar-solicitud/editar-solicitud.component.ts
+++ src/app/editar-solicitud/editar-solicitud.component.ts
@@ -1,18 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
