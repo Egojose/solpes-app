41098615cf4dad6061d8f021fb29014f819a3bbe
Egojose <josemicrobiologo@gmail.com>
2019-12-01 21:55:41 -0500

    se agrega lista responsableSoporte

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -23,5 +23,6 @@ export const environment = {
   listaCausalExcepcion: 'CausalExcepcion',
   listaCompradores: 'Compradores',
   listaHistorial: 'HistorialSuspensiones',
-  listaSolicitudesCrm: 'SolicitudesCrm'
+  listaSolicitudesCrm: 'SolicitudesCrm',
+  listaResponsableSoporte: 'ResponsableSoporte'
 };
